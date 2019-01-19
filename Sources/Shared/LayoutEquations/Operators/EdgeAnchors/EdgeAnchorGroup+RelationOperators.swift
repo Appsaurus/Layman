1b48b2cb100c77e19b69d6498a02b5c749e01ff1
Brian Strobach <brian@appsaurus.io>
2019-01-19 17:18:48 -0600

    Beginning to implement alternative, more verbose API.

--- Sources/Shared/LayoutEquations/Operators/EdgeAnchors/EdgeAnchorGroup+RelationOperators.swift
+++ Sources/Shared/LayoutEquations/Operators/EdgeAnchors/EdgeAnchorGroup+RelationOperators.swift
@@ -4,75 +4,42 @@
 //
 //  Created by Brian Strobach on 1/9/19.
 //  Copyright © 2019 Brian Strobach. All rights reserved.
+
 // MARK: Anchor == Anchor
 @discardableResult
 public func == (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroup) -> SideConstraints {
-    return constraints(lhs, .equal, rhs)
+    return lhs.equal(to: rhs)
 }
 
 // MARK: Equal
 // MARK: Anchor == Expression
 @discardableResult
 public func == (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroupExpression) -> SideConstraints {
-    return constraints(lhs, .equal, rhs)
+    return lhs.equal(to: rhs)
 }
 
 // MARK: Anchor <= Anchor
 @discardableResult
 public func <= (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroup) -> SideConstraints {
-    return constraints(lhs, .lessThanOrEqual, rhs)
+    return lhs.lessThanOrEqual(to: rhs)
 }
 
 // MARK: LessThanOrEqual
 // MARK: Anchor <= Expression
 @discardableResult
 public func <= (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroupExpression) -> SideConstraints {
-    return constraints(lhs, .lessThanOrEqual, rhs)
+    return lhs.lessThanOrEqual(to: rhs)
 }
 
 // MARK: Anchor >= Anchor
 @discardableResult
 public func >= (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroup) -> SideConstraints {
-    return constraints(lhs, .greaterThanOrEqual, rhs)
+    return lhs.greaterThanOrEqual(to: rhs)
 }
 
 // MARK: GreaterThanOrEqual
 // MARK: Anchor >= Expression
 @discardableResult
 public func >= (lhs: EdgeAnchorGroup, rhs: EdgeAnchorGroupExpression) -> SideConstraints {
-    return constraints(lhs, .greaterThanOrEqual, rhs)
-}
-
-private func constraints(_ lhs: EdgeAnchorGroup,
-                         _ relation: Constraint.Relation,
-                         _ rhs: EdgeAnchorGroup) -> SideConstraints {
-    return constraints(
-        .init(lhs.top, relation, rhs.top),
-        .init(lhs.leading, relation, rhs.leading),
-        .init(lhs.bottom, relation, rhs.bottom),
-        .init(lhs.trailing, relation, rhs.trailing)
-    )
-}
-
-private func constraints(_ lhs: EdgeAnchorGroup,
-                         _ relation: Constraint.Relation,
-                         _ rhs: EdgeAnchorGroupExpression) -> SideConstraints {
-    return constraints(
-        .init(lhs.top, relation, rhs.topExpression),
-        .init(lhs.leading, relation, rhs.leadingExpression),
-        .init(lhs.bottom, relation, rhs.bottomExpression),
-        .init(lhs.trailing, relation, rhs.trailingExpression)
-    )
-}
-private func constraints(_ top: LayoutRelationship<YAxisAnchor>,
-                         _ leading: LayoutRelationship<XAxisAnchor>,
-                         _ bottom: LayoutRelationship<YAxisAnchor>,
-                         _ trailing: LayoutRelationship<XAxisAnchor>) -> SideConstraints {
-
-    return SideConstraints(
-        top.constraint,
-        leading.constraint,
-        bottom.constraintInvertedAsInset,
-        trailing.constraintInvertedAsInset
-    )
+    return lhs.greaterThanOrEqual(to: rhs)
 }
