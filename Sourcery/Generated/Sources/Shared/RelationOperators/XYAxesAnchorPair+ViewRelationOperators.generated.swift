358d74102f4cf5b85635cce278774cb4db45f01d
Brian Strobach <brian@appsaurus.io>
2019-02-20 13:36:25 -0600

    Adding missing relation operators to VC

---
+++ Sourcery/Generated/Sources/Shared/RelationOperators/XYAxesAnchorPair+ViewRelationOperators.generated.swift
@@ -0,0 +1,268 @@
+// Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+//
+//  XYAxesAnchorPair+ViewRelationOperators.swift
+//  Layman
+//
+//  Created by Brian Strobach on 1/4/19.
+//  Copyright © 2019 Brian Strobach. All rights reserved.
+//
+
+
+// MARK: View attribute inferred from Anchor
+
+// MARK: View == Anchor
+@discardableResult
+public func .= (lhs: View, rhs: XYAxesAnchorPair) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].equal(to: rhs)
+}
+
+// MARK: View <= Anchor
+@discardableResult
+public func ≤ (lhs: View, rhs: XYAxesAnchorPair) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].lessThanOrEqual(to: rhs)
+}
+
+// MARK: View >= Anchor
+@discardableResult
+public func ≥ (lhs: View, rhs: XYAxesAnchorPair) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].greaterThanOrEqual(to: rhs)
+}
+
+// MARK: View >=< Anchor
+@discardableResult
+public func ≥≤ (lhs: View, rhs: XYAxesAnchorPair) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].inset(from: rhs)
+}
+
+// MARK: View <=> Anchor
+@discardableResult
+public func ≤≥ (lhs: View, rhs: XYAxesAnchorPair) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].outset(from: rhs)
+}
+
+// MARK: View attribute inferred from Anchor collection
+
+// MARK: View == [Anchor]
+@discardableResult
+public func .= (lhs: View, rhs: [XYAxesAnchorPair]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs .= $0})
+}
+
+// MARK: View <= [Anchor]
+@discardableResult
+public func ≤ (lhs: View, rhs: [XYAxesAnchorPair]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≤ $0})
+}
+
+// MARK: View >= [Anchor]
+@discardableResult
+public func ≥ (lhs: View, rhs: [XYAxesAnchorPair]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≥ $0})
+}
+
+// MARK: View >=< [Anchor]
+@discardableResult
+public func ≥≤ (lhs: View, rhs: [XYAxesAnchorPair]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≥≤ $0})
+}
+
+// MARK: View <=> [Anchor]
+@discardableResult
+public func ≤≥ (lhs: View, rhs: [XYAxesAnchorPair]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≤≥ $0})
+}
+
+// MARK: View array attributes inferred from Anchor
+
+// MARK: [View] == Anchor
+@discardableResult
+public func .= (lhs: [View], rhs: XYAxesAnchorPair) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 .= rhs }
+}
+
+// MARK: [View] <= Anchor
+@discardableResult
+public func ≤ (lhs: [View], rhs: XYAxesAnchorPair) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≤ rhs }
+}
+
+// MARK: [View] >= Anchor
+@discardableResult
+public func ≥ (lhs: [View], rhs: XYAxesAnchorPair) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≥ rhs }
+}
+
+// MARK: [View] >=< Anchor
+@discardableResult
+public func ≥≤ (lhs: [View], rhs: XYAxesAnchorPair) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≥≤ rhs }
+}
+
+// MARK: [View] <=> Anchor
+@discardableResult
+public func ≤≥ (lhs: [View], rhs: XYAxesAnchorPair) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≤≥ rhs }
+}
+
+// MARK: View array attributes inferred from Anchor collection
+
+// MARK: [View] == [Anchor]
+@discardableResult
+public func .= (lhs: [View], rhs: [XYAxesAnchorPair]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 .= rhs }
+}
+
+// MARK: [View] <= [Anchor]
+@discardableResult
+public func ≤ (lhs: [View], rhs: [XYAxesAnchorPair]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≤ rhs }
+}
+
+// MARK: [View] >= [Anchor]
+@discardableResult
+public func ≥ (lhs: [View], rhs: [XYAxesAnchorPair]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≥ rhs }
+}
+
+// MARK: [View] >=< [Anchor]
+@discardableResult
+public func ≥≤ (lhs: [View], rhs: [XYAxesAnchorPair]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≥≤ rhs }
+}
+
+// MARK: [View] <=> [Anchor]
+@discardableResult
+public func ≤≥ (lhs: [View], rhs: [XYAxesAnchorPair]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≤≥ rhs }
+}
+
+
+// MARK: View attribute inferred from expression
+
+// MARK: View == Expression
+@discardableResult
+public func .= (lhs: View, rhs: XYAxesAnchorPairExpression) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].equal(to: rhs)
+}
+
+// MARK: View <= Expression
+@discardableResult
+public func ≤ (lhs: View, rhs: XYAxesAnchorPairExpression) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].lessThanOrEqual(to: rhs)
+}
+
+// MARK: View >= Expression
+@discardableResult
+public func ≥ (lhs: View, rhs: XYAxesAnchorPairExpression) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].greaterThanOrEqual(to: rhs)
+}
+
+// MARK: View >=< Expression
+@discardableResult
+public func ≥≤ (lhs: View, rhs: XYAxesAnchorPairExpression) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].inset(from: rhs)
+}
+
+// MARK: View <=> Expression
+@discardableResult
+public func ≤≥ (lhs: View, rhs: XYAxesAnchorPairExpression) -> XYAxesAnchorPair.Solution {
+    return lhs[keyPath: ...rhs].outset(from: rhs)
+}
+
+// MARK: View attribute inferred from expression collection
+
+// MARK: View == Expressions
+@discardableResult
+public func .= (lhs: View, rhs: [XYAxesAnchorPairExpression]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs .= $0})
+}
+
+// MARK: View <= Expressions
+@discardableResult
+public func ≤ (lhs: View, rhs: [XYAxesAnchorPairExpression]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≤ $0})
+}
+
+// MARK: View >= Expressions
+@discardableResult
+public func ≥ (lhs: View, rhs: [XYAxesAnchorPairExpression]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≥ $0})
+}
+
+// MARK: View >=< Expression
+@discardableResult
+public func ≥≤ (lhs: View, rhs: [XYAxesAnchorPairExpression]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≥≤ $0})
+}
+
+// MARK: View <=> Expression
+@discardableResult
+public func ≤≥ (lhs: View, rhs: [XYAxesAnchorPairExpression]) -> [XYAxesAnchorPair.Solution] {
+    return rhs.map({lhs ≤≥ $0})
+}
+
+// MARK: View array attributes inferred from Expression
+
+// MARK: [View] == Expression
+@discardableResult
+public func .= (lhs: [View], rhs: XYAxesAnchorPairExpression) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 .= rhs }
+}
+
+// MARK: [View] <= Expression
+@discardableResult
+public func ≤ (lhs: [View], rhs: XYAxesAnchorPairExpression) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≤ rhs }
+}
+
+// MARK: [View] >= Expression
+@discardableResult
+public func ≥ (lhs: [View], rhs: XYAxesAnchorPairExpression) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≥ rhs }
+}
+
+// MARK: [View] >=< Expression
+@discardableResult
+public func ≥≤ (lhs: [View], rhs: XYAxesAnchorPairExpression) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≥≤ rhs }
+}
+
+// MARK: [View] <=> Expression
+@discardableResult
+public func ≤≥ (lhs: [View], rhs: XYAxesAnchorPairExpression) -> [XYAxesAnchorPair.Solution] {
+    return lhs.map { $0 ≤≥ rhs }
+}
+
+// MARK: View array attributes inferred from expression collection
+
+// MARK: [View] == [Expression]
+@discardableResult
+public func .= (lhs: [View], rhs: [XYAxesAnchorPairExpression]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 .= rhs }
+}
+
+// MARK: [View] <= [Expression]
+@discardableResult
+public func ≤ (lhs: [View], rhs: [XYAxesAnchorPairExpression]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≤ rhs }
+}
+
+// MARK: [View] >= [Expression]
+@discardableResult
+public func ≥ (lhs: [View], rhs: [XYAxesAnchorPairExpression]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≥ rhs }
+}
+
+// MARK: [View] >=< [Expression]
+@discardableResult
+public func ≥≤ (lhs: [View], rhs: [XYAxesAnchorPairExpression]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≥≤ rhs }
+}
+
+// MARK: [View] <=> [Expression]
+@discardableResult
+public func ≤≥ (lhs: [View], rhs: [XYAxesAnchorPairExpression]) -> [[XYAxesAnchorPair.Solution]] {
+    return lhs.map { $0 ≤≥ rhs }
+}
